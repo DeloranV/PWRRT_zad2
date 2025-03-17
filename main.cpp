22b0d96cdef2103a17842e8064e213d9ce97ba09
DeloranV <34947157+DeloranV@users.noreply.github.com>
2025-03-17 11:44:38 +0100

    added point sorting + additional helper functions

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void swapPoints(Point *c1, Point *c2) {
 }
 
 // Szukanie pierwszego punktu
+Point firstPoint;
 void firstCoordinate(Point points[], int pointsElements) {
     double minY = points[0].y;
     int minIndex = 0;
@@ -23,6 +24,37 @@ void firstCoordinate(Point points[], int pointsElements) {
         }
     }
     swapPoints(&points[0], &points[minIndex]);
+    firstPoint = points[0];
+}
+
+// Oblicza kwadrat odległości między punktami
+double calculateDistanceSquared(Point c1, Point c2) {
+    return (c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y);
+}
+
+
+// Kierunek obrotu do każdego z porównywanych punktów
+// 0 -> Kolinearne, 1 -> Zgodny z ruchem zegara, 2 -> Przeciwny ruchowi zegara
+int getTurnDirection(Point p, Point q, Point r) {
+    double value = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (value == 0) return 0;
+    if (value > 0) return 1;
+    return 2;
+}
+
+// Klucz sortowania dla funkcji qsort - sortuje według kąta względem pierwszego punktu
+int compareCoordinates(const void *c1,  const void *c2) {
+    const Point *point1 = static_cast<const Point*>(c1);
+    const Point *point2 = static_cast<const Point*>(c2);
+
+    int direction = getTurnDirection(firstPoint, *point1, *point2);
+
+    if (direction == 0) {
+        if (calculateDistanceSquared(firstPoint, *point2) >= calculateDistanceSquared(firstPoint, *point1)) return -1;
+        return 1;
+    }
+    if (direction == 2) return -1;
+    return 1;
 }
 
 int main() {
@@ -38,6 +70,7 @@ int main() {
     std::cout << std::endl;
 
     firstCoordinate(points, pointsElements);
+    qsort(&points[1], pointsElements - 1, sizeof(Point), compareCoordinates);
 
     for (Point p : points) {
         std::cout << p.x << ' ' << p.y << std::endl;
